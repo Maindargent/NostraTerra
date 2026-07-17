5988705a365817f911a03d676486fed2bd80c2e4
Maindargent <ymoutiez@outlook.fr>
2026-07-17 18:06:15 +0200

    first commit

---
+++ Nostra_Terra/Nostra_TerraApp.swift
@@ -0,0 +1,17 @@
+//
+//  Nostra_TerraApp.swift
+//  Nostra_Terra
+//
+//  Created by Apprenant 87 on 17/07/2026.
+//
+
+import SwiftUI
+
+@main
+struct Nostra_TerraApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
