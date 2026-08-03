6ffe41bfe30fb46a5220deab1053230d2474e812
Moutiez <ymoutiez@outlook.fr>
2026-08-03 15:51:41 +0200

    modification de ProfilEditView

--- Nostra_Terra/Model/User.swift
+++ Nostra_Terra/Model/User.swift
@@ -14,8 +14,8 @@ struct User: Identifiable {
     let birthDate: Date
     let description: String
     let profilPicture: URL
-    let likedPublication: [Publication] = []
-    let publishedPublication: [Publication] = []
+    let likedPublication: [any Publication] = []
+    let publishedPublication: [any Publication] = []
 }
 
 
--- Nostra_Terra/UI/View/ProfileEditView.swift
+++ Nostra_Terra/UI/View/ProfileEditView.swift
@@ -9,7 +9,11 @@ import SwiftUI
 struct ProfileEditView: View {
     @State private var noSound = false
     @State private var messageNoSound = false
+    @State private var name = ""
+    @State private var age = ""
     @State private var texte = ""
+    @State private var profilPictule = ""
+    let user: User
     
     var body: some View {
         ZStack{
@@ -33,54 +37,104 @@ struct ProfileEditView: View {
                     .font(.title2)
                     .padding(.horizontal)
                 ZStack{
-                    Divider()
-                        .frame(height: 4)
-                        .overlay(.whiteIvoryMist)
                     HStack{
-                        
-                        Image("breton")
-                            .frame(width:100, height:100)
-                            .clipShape(Circle())
-                            .overlay(
-                                Circle()
-                                    .stroke(.whiteIvoryMist, lineWidth: 3)
-                            )
-                            .cornerRadius(400)
-                            .padding(.leading)
-                            .padding(.vertical)
-                        
-                        TextField("Martin Dupont", text: $texte)
-                            .padding(5)
-                            .background(.white)
-                            .cornerRadius(20)
-                        
-                        TextField("34", text: $texte)
-                            .padding(5)
-                            .background(.white)
-                            .cornerRadius(20)
+                        AsyncImage(url:
+                                    user.profilPicture) { image in
+                            image
+                                .resizable()
+                                .scaledToFill()
+                                .frame(width:100, height:100)
+                                .clipShape(Circle())
+                                .cornerRadius(20)
+                                .padding(.horizontal, 5)
+                                .overlay(
+                                    Circle()
+                                        .stroke(.whiteIvoryMist, lineWidth: 3)
+                                )
+                        } placeholder: {
+                            ProgressView()
+                        }
+                        //                        Image("breton")
+                        //                            .frame(width:100, height:100)
+                        //                            .clipShape(Circle())
+                        //                            .overlay(
+                        //                                Circle()
+                        //                                    .stroke(.whiteIvoryMist, lineWidth: 3)
+                        //                            )
+                        //                            .cornerRadius(400)
+                        //                            .padding(.leading)
+                        //                            .padding(.vertical)
+                        VStack{
+                            HStack{
+                                TextField(text: $name) {
+                                    Text("\(user.firstName) \(user.lastName)")
+                                        .foregroundStyle(.white.opacity(0.5))
+                                }
+                                    .padding(.horizontal, 9)
+                                    .padding(.vertical, 3)
+                                    .glassEffect(.clear, in: RoundedRectangle(cornerRadius: 12))
+                                    .foregroundStyle(.whiteIvoryMist)
+                                //faire le calcul pour afficher l'age
+                                TextField(text: $age) {
+                                    Text("\(user.birthDate)")
+                                        .foregroundStyle(.white.opacity(0.5))
+                                }
+                                    .padding(.horizontal, 9)
+                                    .padding(.vertical, 3)
+                                    .frame(width: 50)
+                                    .glassEffect(.clear, in: RoundedRectangle(cornerRadius: 12))
+                                    .foregroundStyle(.whiteIvoryMist)
+                            }
+                            .padding(.vertical, 8)
+                            TextField(text: $profilPictule) {
+                                Text("\(user.profilPicture)")
+                                    .foregroundStyle(.white.opacity(0.5))
+                            }
+                                .padding(.horizontal, 9)
+                                .padding(.vertical, 3)
+                                .glassEffect(.clear, in: RoundedRectangle(cornerRadius: 12))
+                                .foregroundStyle(.whiteIvoryMist)
+                                .padding(.vertical, 8)
+                        }
                     }
                     .padding()
                 }
-                    TextField("Waffles attack like a vicious monster but use lap as chair stare at ceiling tweeting a baseball. Pee on walls it smells like breakfast fooled again thinking the dog likes me for kitty power so find empty spot in cupboard and sleep all day so cat gets stuck in tree firefighters try to get cat down firefighters get stuck in tree cat eats", text: $texte, axis: .vertical)
-                        .lineLimit(8...10)
-                        .frame(minHeight: 200)
-                    //            TextEditor(text: "Waffles attack like a vicious monster but use lap as chair stare at ceiling tweeting a baseball. Pee on walls it smells like breakfast fooled again thinking the dog likes me for kitty power so find empty spot in cupboard and sleep all day so cat gets stuck in tree firefighters try to get cat down firefighters get stuck in tree cat eats", selection: $texte)
-                        .padding(5)
-                        .background(.white)
-                        .cornerRadius(20)
-                        .padding()
+                TextField(text: $texte, axis: .vertical) {
+                    Text("Waffles attack like a vicious monster but use lap as chair stare at ceiling tweeting a baseball. Pee on walls it smells like breakfast fooled again thinking the dog likes me for kitty power so find empty spot in cupboard and sleep all day so cat gets stuck in tree firefighters try to get cat down firefighters get stuck in tree cat eats")
+                        .foregroundStyle(.white.opacity(0.5))
                 }
-            
+                .lineLimit(8...10)
+                .frame(minHeight: 200)
+                .padding(.horizontal, 9)
+                .padding(.vertical, 3)
+                .glassEffect(.clear, in: RoundedRectangle(cornerRadius: 12))
+                .foregroundStyle(.whiteIvoryMist)
+                .padding()
+                Button() {
+                    
+                } label: {
+                    Image(systemName: "checkmark")
+                    Text("Valider")
+                }
+                .padding(8)
+                .glassEffect(.clear, in: RoundedRectangle(cornerRadius: 12))
+                .foregroundStyle(.whiteIvoryMist)
+            }
+        }
+    }
+}
+
+#Preview {
+    ProfileEditView(user: users[0])
+}
+
+//                    Divider()
+//                        .frame(height: 4)
+//                        .overlay(.whiteIvoryMist)
+
 //            .background {
 //                Image(.backgroundPicture)
 //                    .resizable()
 //                    .frame(maxWidth: .infinity, maxHeight: .infinity)
 //                    .ignoresSafeArea()
 //            }
-        }
-    }
-}
-    
-#Preview {
-    ProfileEditView()
-}
