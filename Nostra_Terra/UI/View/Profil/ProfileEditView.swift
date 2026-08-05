2ebd34bfb78cc09eeb446d891f94996abb7cd2ad
Moutiez <ymoutiez@outlook.fr>
2026-08-05 14:33:57 +0200

    test

--- Nostra_Terra/UI/View/Profil/ProfileEditView.swift
+++ Nostra_Terra/UI/View/Profil/ProfileEditView.swift
@@ -5,6 +5,7 @@
 //  Created by Jules Liegeois on 23/07/2026.
 //
 import SwiftUI
+import PhotosUI
 
 struct ProfileEditView: View {
     @State private var noSound = false
@@ -13,6 +14,9 @@ struct ProfileEditView: View {
     @State private var age = ""
     @State private var texte = ""
     @State private var profilPictule = ""
+    
+    @State private var avatarItem: PhotosPickerItem?
+    @State private var avatarImage: Image?
     let user: User
     
     var body: some View {
@@ -38,8 +42,7 @@ struct ProfileEditView: View {
                     .padding(.horizontal)
                 ZStack{
                     HStack{
-                        AsyncImage(url:
-                                    user.profilPicture) { image in
+                        AsyncImage(url: user.profilPicture) { image in
                             image
                                 .resizable()
                                 .scaledToFill()
@@ -54,50 +57,58 @@ struct ProfileEditView: View {
                         } placeholder: {
                             ProgressView()
                         }
-                        //                        Image("breton")
-                        //                            .frame(width:100, height:100)
-                        //                            .clipShape(Circle())
-                        //                            .overlay(
-                        //                                Circle()
-                        //                                    .stroke(.whiteIvoryMist, lineWidth: 3)
-                        //                            )
-                        //                            .cornerRadius(400)
-                        //                            .padding(.leading)
-                        //                            .padding(.vertical)
                         VStack{
                             HStack{
                                 TextField(text: $name) {
                                     Text("\(user.firstName) \(user.lastName)")
                                         .foregroundStyle(.white.opacity(0.5))
                                 }
-                                    .padding(.horizontal, 9)
-                                    .padding(.vertical, 3)
-                                    .glassEffect(.clear, in: RoundedRectangle(cornerRadius: 12))
-                                    .foregroundStyle(.whiteIvoryMist)
+                                .padding(.horizontal, 9)
+                                .padding(.vertical, 3)
+                                .glassEffect(.clear, in: RoundedRectangle(cornerRadius: 12))
+                                .foregroundStyle(.whiteIvoryMist)
+                                
                                 //faire le calcul pour afficher l'age
                                 TextField(text: $age) {
                                     Text("\(user.birthDate)")
                                         .foregroundStyle(.white.opacity(0.5))
                                 }
-                                    .padding(.horizontal, 9)
-                                    .padding(.vertical, 3)
-                                    .frame(width: 50)
-                                    .glassEffect(.clear, in: RoundedRectangle(cornerRadius: 12))
-                                    .foregroundStyle(.whiteIvoryMist)
-                            }
-                            .padding(.vertical, 8)
-                            TextField(text: $profilPictule) {
-                                Text("\(user.profilPicture)")
-                                    .foregroundStyle(.white.opacity(0.5))
-                            }
+                                
                                 .padding(.horizontal, 9)
                                 .padding(.vertical, 3)
+                                .frame(width: 50)
                                 .glassEffect(.clear, in: RoundedRectangle(cornerRadius: 12))
                                 .foregroundStyle(.whiteIvoryMist)
-                                .padding(.vertical, 8)
+                            }
+                            .padding(.vertical, 8)
+                            
+                            PhotosPicker("Select avatar", selection: $avatarItem, matching: .images)
+                            
+                            avatarImage?
+                                .resizable()
+                                .scaledToFill()
+                                .frame(width:100, height:100)
+                                .clipShape(Circle())
+                                .cornerRadius(20)
+                                .padding(.horizontal, 5)
+                                .overlay(
+                                    Circle()
+                                        .stroke(.whiteIvoryMist, lineWidth: 3)
+                                )
+                            
+                            .onChange(of: avatarItem) {
+                                Task {
+                                    if let loaded = try? await avatarItem?.loadTransferable(type: Image.self) {
+                                        avatarImage = loaded
+                                    } else {
+                                        print("Failed")
+                                    }
+                                }
+                            }
                         }
                     }
-                    .padding()
+                    .padding(8)
+                    .padding(.horizontal, 8)
                 }
                 TextField(text: $texte, axis: .vertical) {
                     Text("Waffles attack like a vicious monster but use lap as chair stare at ceiling tweeting a baseball. Pee on walls it smells like breakfast fooled again thinking the dog likes me for kitty power so find empty spot in cupboard and sleep all day so cat gets stuck in tree firefighters try to get cat down firefighters get stuck in tree cat eats")
