c52541576c61cd689d37b5cbdfcd3d531a0a514f
ShoSho <coudray.shona@icloud.com>
2026-08-07 16:48:04 +0200

    beginning the search view

--- Nostra_Terra/UI/View/Search/SearchView.swift
+++ Nostra_Terra/UI/View/Search/SearchView.swift
@@ -8,11 +8,62 @@
 import SwiftUI
 
 struct SearchView: View {
+    
+    @Environment(PublicationViewModel.self) var publicationsManager
+    @State var searchResults: [any Publication] = []
+    @State var searchQuery: String = ""
+    
+    var isSearching: Bool {
+        return !searchQuery.isEmpty
+    }
+    
     var body: some View {
-        Text("Shona's view")
+        NavigationStack {
+            List{
+                if isSearching {
+                    ForEach(searchResults, id: \.id) { result in
+                        Text(result.title)
+                    }
+                } else {
+//                    ForEach(publicationsManager.getPublications(), id: \.id) { publication in
+//                        Text(publication.title)
+//                    }
+                }
+            }
+            .searchable(
+                text: $searchQuery,
+                placement: .automatic,
+                prompt: "Recherche..."
+            )
+            .textInputAutocapitalization(.never)
+            .onChange(of: searchQuery) {
+                self.fetchSearchResults()
+            }
+            .onSubmit(of: .search) {
+                fetchSearchResults()//for: searchQuery)
+            }
+            .overlay {
+                if isSearching && searchResults.isEmpty {
+                    ContentUnavailableView(
+                        "Aucun résultat pour \(searchQuery)",
+                        systemImage: "magnifyingglass",
+                        description: Text("Vérifiez l'orthographe ou lancez une nouvelle recherche.")
+                    )
+                }
+            }
+        }
+    }
+    private func fetchSearchResults(){//for query: String) {
+        searchResults = publicationsManager.getPublications().filter { publication in
+            publication.title
+                .lowercased()
+                .contains(searchQuery)
+        }
     }
 }
 
 #Preview {
+    @Previewable @State var publicationManager = PublicationViewModel()
     SearchView()
+        .environment(publicationManager)
 }
