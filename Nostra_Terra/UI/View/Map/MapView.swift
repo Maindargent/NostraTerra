e5ae30a3ea7bc8d64ef4a87bbd926bab46d281fb
Jules <julesliegeois@gmail.com>
2026-08-11 19:43:12 +0200

    feat: filtre categories

--- Nostra_Terra/UI/View/Map/MapView.swift
+++ Nostra_Terra/UI/View/Map/MapView.swift
@@ -32,6 +32,8 @@ struct MapView: View {
     @State private var selectedCategory: PublicationCategory?
     @State private var selectedRegion: FrenchRegion?
     
+    @State var showFilterCategories: Bool = false
+    
     var body: some View {
         Map(
             position: $cameraPosition,
@@ -69,11 +71,12 @@ struct MapView: View {
         .mapControls {
             MapUserLocationButton()
         }
+        //MARK: SELECTED PUBLICATION
         .onChange(of: selectedPublicationID) { _, publicationID in
             guard
                 let publicationID,
                 let publication = publicationsManager.getPublication(id: publicationID)
-                    else {
+            else {
                 return
             }
             
@@ -101,40 +104,105 @@ struct MapView: View {
                 PublicationDetailView(
                     publicationID: selectedPublicationID!
                 ).environment(userViewModel)
-                .toolbar(content: {
-                    ToolbarItem(placement: .bottomBar) {
-                        Button {
-                            selectedPublicationID = nil
-                            showPublicationSheet.toggle()
-                        } label: {
-                            Image(systemName: "arrowshape.turn.up.backward")
-                                .foregroundStyle(.white)
+                    .toolbar(content: {
+                        ToolbarItem(placement: .bottomBar) {
+                            Button {
+                                selectedPublicationID = nil
+                                showPublicationSheet.toggle()
+                            } label: {
+                                Image(systemName: "arrowshape.turn.up.backward")
+                                    .foregroundStyle(.white)
+                            }
+                            .padding()
+                            .buttonStyle(.glassProminent)
+                            .glassEffect(.clear, in: .circle)
                         }
-                        .padding()
-                        .buttonStyle(.glassProminent)
-                        .glassEffect(.clear, in: .circle)
-                    }
-                    .sharedBackgroundVisibility(.hidden)
-                })
-                .presentationDragIndicator(.visible)
-                .presentationDetents([.fraction(0.8), .large])
-                .presentationBackground(.clear)
+                        .sharedBackgroundVisibility(.hidden)
+                    })
+                    .presentationDragIndicator(.visible)
+                    .presentationDetents([.fraction(0.8), .large])
+                    .presentationBackground(.clear)
             }
         )
+        //MARK: FILTER BY REGION & CATEGORIES
         .overlay(alignment: .topLeading) {
-            HStack {
+            VStack(alignment: .leading, spacing: 8) {
+                VStack(alignment: .leading) {
+                    //MARK: FILTER BY CATEGORIES
+                    if showFilterCategories {
+                        VStack(alignment: .leading) {
+                            
+                            Button {
+                                withAnimation {
+                                    selectedCategory = nil
+                                    showFilterCategories.toggle()
+                                }
+                            } label: {
+                                HStack(spacing: 6) {
+                                    Text("Toutes")
+                                        .foregroundStyle(.white)
+                                        .padding(4)
+                                }
+                            }
+                            .padding(4)
+                            .foregroundStyle(.white)
+                            
+                            ForEach(PublicationCategory.allCases) { cat in
+                                Button {
+                                    withAnimation {
+                                        selectedCategory = cat
+                                        selectedRegion = nil
+                                        showFilterCategories.toggle()
+                                    }
+                                } label: {
+                                    HStack(spacing: 6) {
+                                        Text(cat.rawValue)
+                                            .foregroundStyle(.white)
+                                            .padding(4)
+                                        if selectedCategory == cat {
+                                            Image(systemName: "checkmark")
+                                        }
+                                    }
+                                    .padding(4)
+                                    .foregroundStyle(.white)
+                                }
+                            }
+                        }
+                        .padding(8)
+                    } else {
+                        Button {
+                            withAnimation {
+                                showFilterCategories.toggle()
+                            }
+                        } label: {
+                            HStack(spacing: 6) {
+                                Text("\(selectedCategory?.rawValue ?? "Catégories")")
+                                Image(systemName: "line.3.horizontal.decrease")
+                            }
+                            .foregroundStyle(.white)
+                        }
+                        .padding(12)
+                    }
+                }
+                .glassEffect(.regular, in: .rect(cornerRadius: 24))
+                
                 Button {
                     showFilterRegionSheet.toggle()
                 } label: {
-                    Image(systemName: "line.3.horizontal.decrease")
-                        .padding(12)
+                    HStack(spacing: 6) {
+                        Text("\(selectedRegion?.titre ?? "Régions")")
+                        Image(.franceIcon)
+                            .resizable()
+                            .frame(width: 20, height: 20, alignment: .center)
+                    }
+                    .padding(4)
                 }
                 .buttonStyle(.glassProminent)
                 .tint(.clear)
-                .buttonBorderShape(.circle)
             }
             .padding()
         }
+        //MARK: FILTER BY REGION
         .sheet(isPresented: $showFilterRegionSheet, content: {
             FilterPublicationView(
                 selectedCategory: $selectedCategory,
@@ -152,7 +220,7 @@ struct MapView: View {
             
             if showedPublication.count == 1 {
                 
-                var region = MKCoordinateRegion(
+                let region = MKCoordinateRegion(
                     center: showedPublication[0].geoPoint,
                     latitudinalMeters: 100_000,
                     longitudinalMeters: 100_000
